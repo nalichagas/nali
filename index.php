6104db4f647ea30224fc1febba7a082c51e50799
nali <nathaly.c.paula@aluno.senai.br>
2026-09-21 14:15:59 -0300

    Change title and heading to 'Taylor Swift'

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hi haters</title>
+    <title>Taylor Swift</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 <div class="card">
     <?php
-    echo "<h1> Hi haters </h1>";
+    echo "<h1> Taylor Swift </h1>";
 
     $nome = $_POST['nome'] ?? '';
     $idade = $_POST['idade'] ?? null;
